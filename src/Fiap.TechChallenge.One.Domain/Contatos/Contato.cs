d7acaffaaf534f75212ca190c431471af25f6609
Gabriel Porto <gr_porto@hotmail.com>
2024-04-10 21:13:01 -0300

    add propriedades a classe de contato

--- src/Fiap.TechChallenge.One.Domain/Contatos/Contato.cs
+++ src/Fiap.TechChallenge.One.Domain/Contatos/Contato.cs
@@ -4,5 +4,28 @@ namespace Fiap.TechChallenge.One.Domain.Contatos;
 
 public sealed class Contato : Entity
 {
+    private Contato(
+        Guid id,
+        Nome nome,
+        Email email)
+    {
+        Id = id;
+        Nome = nome;
+        Email = email;
+    }
 
+    public Nome Nome { get; private set; }
+
+    public Email Email { get; private set; }
+
+    //TODO: Criar objeto de valor para telefone
+
+    public static Result<Contato> Criar(
+        Nome nome, Email email)
+    {
+        return new Contato(
+            Guid.NewGuid(),
+            nome,
+            email);
+    }
 }
