e1a326bab21aac7f120c8d9b8c6d2c88102e71ea
Gabriel Porto <gr_porto@hotmail.com>
2024-04-12 22:50:38 -0300

    feat: Add consulta de contato por id

---
+++ src/Fiap.TechChallenge.One.Application/Contatos/ObterPorId/ObterContatoPorIdQuery.cs
@@ -0,0 +1,22 @@
+﻿using Fiap.TechChallenge.One.Application.Abstractions.Messaging;
+using Fiap.TechChallenge.One.Domain.Contatos;
+
+namespace Fiap.TechChallenge.One.Application.Contatos.ObterPorId;
+
+public sealed record ObterContatoPorIdQuery(Guid ContatoId) : IQuery<ContatoResponse>;
+
+public sealed record ContatoResponse(
+    Guid Id,
+    string Nome,
+    string Email,
+    string Telefone)
+{
+    public static ContatoResponse ContatoParaContatoResponse(Contato contato)
+    {
+        return new ContatoResponse(
+            contato.Id,
+            contato.Nome.Value,
+            contato.Email.Value,
+            contato.Telefone.Value);
+    }
+};
