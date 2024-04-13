e80de14e03e3c5abb98884294d1c54592f42e22b
Gabriel Porto <gr_porto@hotmail.com>
2024-04-12 22:50:51 -0300

    tests: add teste para consulta de contato por id

---
+++ tests/Application.UnitTests/Contatos/ObterContatoPorIdQueryTests.cs
@@ -0,0 +1,61 @@
+﻿using Fiap.TechChallenge.One.Application.Contatos.ObterPorId;
+using Fiap.TechChallenge.One.Domain.Contatos;
+using Fiap.TechChallenge.One.Domain.Kernel;
+using FluentAssertions;
+using NSubstitute;
+
+namespace Application.UnitTests.Contatos;
+
+public class ObterContatoPorIdQueryTests
+{
+    private static readonly Contato Contato = Contato.Criar(
+        Nome.Criar("Gabriel Teste").Value,
+        Email.Criar("gabriel.test@test.com").Value,
+        Telefone.Criar("987654321").Value).Value;
+
+    private static readonly ObterContatoPorIdQuery Query = new(
+       Contato.Id);
+
+    private readonly ObterContatoPorIdQueryHandler _handler;
+    private readonly IContatoRepository _contatoRepositoryMock;
+
+    public ObterContatoPorIdQueryTests()
+    {
+        _contatoRepositoryMock = Substitute.For<IContatoRepository>();
+
+        _handler = new(_contatoRepositoryMock);
+    }
+
+    [Fact]
+    public async Task Handle_DeveRetornarErro_QuandoContatoNaoExiste()
+    {
+        // Arrange
+        _contatoRepositoryMock.ObterPorIdAsync(
+            Arg.Is<Guid>(c => c == Query.ContatoId), Arg.Any<CancellationToken>())
+            .Returns((Contato?)null);
+
+        // Act
+        Result<ContatoResponse> result = await _handler.Handle(Query, default);
+
+        // Assert
+        result.IsFailure.Should().BeTrue();
+        result.Error.Should().Be(ContatoErrors.NaoEncontrado(Query.ContatoId));
+    }
+
+    [Fact]
+    public async Task Handle_DeveRetornarSucesso_QuandoContatoExiste()
+    {
+        // Arrange
+        _contatoRepositoryMock.ObterPorIdAsync(
+            Arg.Is<Guid>(c => c == Query.ContatoId), Arg.Any<CancellationToken>())
+            .Returns(Contato);
+
+        // Act
+        Result<ContatoResponse> result = await _handler.Handle(Query, default);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().NotBeNull();
+        result.Value.Id.Should().Be(Contato.Id);
+    }
+}
