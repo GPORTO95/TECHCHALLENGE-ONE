6409ee15744268c96e885367d553efe9d81c2067
Gabriel Porto <gr_porto@hotmail.com>
2024-04-13 23:59:02 -0300

    feat: criado entidade de Ddd e value object para estado com valor e descricao

---
+++ src/Fiap.TechChallenge.One.Domain/Ddds/Ddd.cs
@@ -0,0 +1,22 @@
+﻿using Fiap.TechChallenge.One.Domain.Kernel;
+
+namespace Fiap.TechChallenge.One.Domain.Ddds;
+
+public sealed class Ddd : Entity
+{
+    private Ddd(
+        Guid id, Estado estado)
+    {
+        Id = id;
+        Estado = estado;
+    }
+
+    public Estado Estado { get; private set; }
+
+    public static Ddd Criar(Estado estado)
+    {
+        return new Ddd(
+            Guid.NewGuid(),
+            estado);
+    }
+}
---
+++ tests/Domain.UnitTests/Ddds/EstadoTests.cs
@@ -0,0 +1,65 @@
+﻿using Fiap.TechChallenge.One.Domain.Contatos;
+using Fiap.TechChallenge.One.Domain.Ddds;
+using FluentAssertions;
+using System.Drawing;
+
+namespace Domain.UnitTests.Ddds;
+
+public class EstadoTests
+{
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("  ")]
+    public void Criar_DeveRetornarErro_QuandoSiglaEhVazio(string valor)
+    {
+        // Arrange
+        // Act
+        var estado = Estado.Criar(valor, "São Paulo");
+
+        // Assert
+        estado.IsFailure.Should().BeTrue();
+        estado.Error.Should().Be(EstadoErrors.Vazio("Sigla"));
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("  ")]
+    public void Criar_DeveRetornarErro_QuandoDescricaoEhVazio(string valor)
+    {
+        // Arrange
+        // Act
+        var estado = Estado.Criar("SP", valor);
+
+        // Assert
+        estado.IsFailure.Should().BeTrue();
+        estado.Error.Should().Be(EstadoErrors.Vazio("Descrição"));
+    }
+
+    [Theory]
+    [InlineData("S")]
+    [InlineData("SSP")]
+    [InlineData("SPPP")]
+    public void Criar_DeveRetornarErro_QuandoSiglaEhInvalido(string valor)
+    {
+        // Arrange
+        // Act
+        var estado = Estado.Criar(valor, "São Paulo");
+
+        // Assert
+        estado.IsFailure.Should().BeTrue();
+        estado.Error.Should().Be(EstadoErrors.TamanhoInvalido);
+    }
+
+    [Fact]
+    public void Criar_DeveRetornarSucesso_QuandoEhValido()
+    {
+        // Arrange
+        // Act
+        var estado = Estado.Criar("SP", "São Paulo");
+
+        // Assert
+        estado.IsSuccess.Should().BeTrue();
+    }
+}
