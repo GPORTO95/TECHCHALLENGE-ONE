81f1dc8d6b95f2c5df51a5e625dff707727bb71c
Gabriel Porto <gr_porto@hotmail.com>
2024-05-06 22:13:46 -0300

    tests: created baseFunctional and functionaltest

---
+++ tests/Api.IntegrationTests/Abstractions/FunctionalTestWebAppFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Api.IntegrationTests.Abstractions;
+
+public class FunctionalTestWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+
+    public Task InitializeAsync()
+    {
+        return Task.CompletedTask;
+    }
+
+    Task IAsyncLifetime.DisposeAsync()
+    {
+        return Task.CompletedTask;
+    }
+}
