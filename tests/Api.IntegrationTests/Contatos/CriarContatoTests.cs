6098ef29be5dacb766871ef70ed041f7ce36cccd
Gabriel Porto <gr_porto@hotmail.com>
2024-05-26 16:22:41 -0300

    add teste de integração para criação de contato

---
+++ tests/Api.IntegrationTests/Contatos/CriarContatoTests.cs
@@ -0,0 +1,27 @@
+﻿using Api.IntegrationTests.Abstractions;
+using Fiap.TechChallenge.One.Application.Contatos.Criar;
+using FluentAssertions;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace Api.IntegrationTests.Contatos;
+
+public class CriarContatoTests : BaseFunctionalTests
+{
+    public CriarContatoTests(FunctionalTestWebAppFactory factory) : base(factory)
+    {
+    }
+
+    [Fact]
+    public async Task Deve_RetornarBadRequest_QuandoEmailEhVazio()
+    {
+        // Arrange
+        CriarContatoCommand request = new("", "Nome Completo", "956789087", "11");
+
+        // Act
+        HttpResponseMessage response = await HttpClient.PostAsJsonAsync("api/v1/contatos", request);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+}
